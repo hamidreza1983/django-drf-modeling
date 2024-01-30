958db75acea87c2c1df5d0a2fbf102543a9e5e0a
mohadese doosti <137603928+mhidusti@users.noreply.github.com>
2024-01-30 23:43:09 +0330

    3

--- restaurant/api/V1/views.py
+++ restaurant/api/V1/views.py
@@ -1,7 +1,6 @@
 
 from .serializer import *
 from ...models import *
-from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from rest_framework.permissions import IsAdminUser, IsAuthenticated, IsAuthenticatedOrReadOnly
 
