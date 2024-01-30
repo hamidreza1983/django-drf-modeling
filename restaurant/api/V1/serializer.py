d0519d02a35e01c3c053b6d5c198216fd2de71e9
MaryamKalantarii <maryamnkalantaria72@gmail.com>
2024-01-30 23:42:11 +0330

    .

--- restaurant/api/V1/serializer.py
+++ restaurant/api/V1/serializer.py
@@ -8,11 +8,7 @@ from ...models import *
 
 
 class ServiceSerializer(serializers.ModelSerializer):
-    
-    # def create(self, validated_data):
-    #     validated_data['user'] = self.context.get('request').user
-    #     return super().create(validated_data)    
-
+   
     class Meta:
         model = Service
         fields =[
