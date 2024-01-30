95d8e982b2cd9bac01383e4955482d4d6c26a23a
Ayda <aydaghasemi66@gmail.com>
2024-01-30 17:34:54 +0330

    ayda ghaviiii

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+
+admin.site.register(Skills)
+admin.site.register(ContactUs)
+admin.site.register(Category)
+admin.site.register(Chief)
+admin.site.register(Services)
+admin.site.register(Food)
--- restaurant/models.py
+++ restaurant/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 # Create your models here.
-class Services (models.Model):
+class Services(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_date= models.DateTimeField(auto_now_add=True)
