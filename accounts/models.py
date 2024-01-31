1c6097683d8019acd5ab4d45213d332319083f83
Sobhan Moradi <137515610+Sobhanpy@users.noreply.github.com>
2024-01-31 08:30:05 +0330

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,73 +1,39 @@
 from django.db import models
-from accounts.models import CustomeUser
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager,PermissionsMixin
 
-
-class Service(models.Model):
-    title = models.CharField(max_length=100)
-    content =models.CharField(max_length=200)
-    status = models.BooleanField(default=False)
-    def __str__(self):
-        return self.title
-
-class Category(models.Model):
-    name = models.CharField(max_length=100)
-    def __str__(self):
-        return self.name
+class CustomeBaseUserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
     
-class Food(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.CharField(max_length=255)
-    category = models.ManyToManyField(Category)
-    price = models.IntegerField(default=0)
-    image = models.ImageField(upload_to='Food',default='defultjpg')
-    status = models.BooleanField(default=False)
-    def __str__(self):
-        return self.title
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self.create_user(email, password, **extra_fields)
 
-class Party(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.CharField(max_length=255)
-    price = models.IntegerField(default=0)
-    image = models.ImageField(upload_to='Party',default='defult.jpg')
-    status = models.BooleanField(default=False)
-    def __str__(self):
-        return self.title
-    
-class Order(models.Model):
-    email = models.EmailField()
-    name = models.CharField(max_length=100)
-    phone = models.CharField(max_length=11)
-    number= models.IntegerField()
-    message = models.TextField()
-    date = models.DateTimeField()
-    time= models.DateTimeField()
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    username  = models.CharField(max_length=100, unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    is_verified = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    objects = CustomeBaseUserManager()
+
     def __str__(self):
         return self.email
-    
-class Skills(models.Model):
-    name = models.CharField(max_length=100)
-    def __str__(self):
-        return self.name
-
-class Chef(models.Model):
-    name = models.CharField(max_length=100)
-    info = models.ForeignKey(CustomeUser , on_delete=models.CASCADE)
-    skills = models.ManyToManyField(Skills)
-    image = models.ImageField(upload_to='Chef', default='defult.jpg')
-    instagram = models.CharField(max_length=255)
-    facebook = models.CharField(max_length=255)
-    linkdin = models.CharField(max_length=255)
-    twitter = models.CharField(max_length=255)
-    status = models.BooleanField(default=False)
-    updated_date = models.DateTimeField(auto_now=True)
-    def __str__(self):
-        return self.info.name
-
-class ContactUs(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    subject = models.CharField(max_length=200)
-    message = models.TextField()
-    def __str__(self):
-        return self.name
     
